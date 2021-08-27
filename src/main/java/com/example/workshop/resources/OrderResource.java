509f687562672cebf977427ce258aba987597ff7
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-08-27 00:38:33 -0300

    Order insert, update and exception handling

--- src/main/java/com/example/workshop/resources/OrderResource.java
+++ src/main/java/com/example/workshop/resources/OrderResource.java
@@ -1,13 +1,18 @@
 package com.example.workshop.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.workshop.entities.Order;
 import com.example.workshop.services.OrderService;
@@ -29,6 +34,19 @@ public class OrderResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Order> insert(@RequestBody Order obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj) {
+		service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/example/workshop/services/OrderService.java
+++ src/main/java/com/example/workshop/services/OrderService.java
@@ -3,11 +3,14 @@ package com.example.workshop.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshop.entities.Order;
 import com.example.workshop.repositories.OrderRepository;
+import com.example.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -21,6 +24,20 @@ public class OrderService {
 	
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+	}
+	
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+	
+	public Order update(Long id, Order obj) {
+		try {
+			Order entity = repository.getOne(id);
+			entity.setOrderStatus(obj.getOrderStatus());
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }
